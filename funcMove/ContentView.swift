a376074d0b30eafdbf4ff237aef24fd7a1661123
Joseph DeWeese <inclinedtactics@icloud.com>
2024-04-17 16:45:57 -0500

    Initial Commit

---
+++ funcMove/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  funcMove
+//
+//  Created by Joseph DeWeese on 4/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ funcMove/funcMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  funcMoveApp.swift
+//  funcMove
+//
+//  Created by Joseph DeWeese on 4/17/24.
+//
+
+import SwiftUI
+
+@main
+struct funcMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
